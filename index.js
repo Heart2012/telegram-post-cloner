4880be1778b1c9570bd49abe81655649d81f00a6
Heart2012 <tagaev.p.a@gmail.com>
2026-08-10 14:31:17 +0300

    Show create and delete buttons in links menu

--- index.js
+++ index.js
@@ -2,5 +2,6 @@
 // Startup helpers are loaded before core so the stable core remains unchanged.
 require("./startup-fix.js");
 require("./language.js");
+require("./links-ui-fix.js");
 require("./link-settings-live2.js");
 require("./core.js");
