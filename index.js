000ab13df1eae19cdd6e29627713f9e3156b62aa
Heart2012 <tagaev.p.a@gmail.com>
2026-08-10 17:09:33 +0300

    Simplify startup chain and remove conflicting runtime patches

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-// Stable startup chain.
-require("./startup-fix.js");
-require("./startup-order-fix.js");
+require("dotenv").config();
 require("./language.js");
-require("./links-ui-fix.js");
-require("./link-settings-live2.js");
-require("./forwarder-hook.js");
 require("./core.js");
